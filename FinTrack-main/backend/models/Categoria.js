42077fc7888076a98a70a072ba3dd228bb22676a
baNanoIT <jealsanchezto@ittepic.edu.mx>
2025-05-11 22:51:45 -0700

    Actualización Usuarios Categorias

--- FinTrack-main/backend/models/Categoria.js
+++ FinTrack-main/backend/models/Categoria.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 
 const CategoriaSchema = new mongoose.Schema({
-  Id_Categoria: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId() },
   Titulo: { type: String, required: true },
   Descripcion: { type: String },
 });
 
-// AQUI CREAMOS el modelo y lo exportamos
-const Categoria = mongoose.model('Categoria', CategoriaSchema);
-
-module.exports = Categoria;
+module.exports = CategoriaSchema;
